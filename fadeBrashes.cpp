29adb7fe2d9ec2a87051d9d7650d628cb2700793
User2021-afk <davidmatry2007@gmail.com>
2021-02-14 18:42:21 +0300

    Update fadeBrashes.cpp

--- fadeBrashes.cpp
+++ fadeBrashes.cpp
@@ -1,16 +1,18 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+// Плавное изменение яркости светодилда
+// V 1.0
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 void setup() {
   Serial.begin(9600);
   pinMode(10, OUTPUT);
 }
-
 void loop() {
-  static int up = 0;
-  static int steps = 5;
-  analogWrite(10, up);
-  up += steps;
+   static int up = 0;
+   static int steps = 5;
+   analogWrite(10, up);
+   up += steps;
   if (up <= 0 || up >= 255) {
-    steps = -steps;
+     steps =- steps;
   }
-  Serial.println(up);
   delay(50);
 }
