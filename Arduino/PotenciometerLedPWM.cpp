512a539a18632f0b71f423e955753e8e7ecafa06
User2021-afk <davidmatry2007@gmail.com>
2021-02-16 17:07:42 +0300

    Abded repo

--- Arduino/PotenciometerLedPWM.cpp
+++ Arduino/PotenciometerLedPWM.cpp
@@ -1,25 +1,20 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
-// Паочерёдное зажигание светодиодов
+// Плавное изменение яркости светодилда
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
-void setup() {
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+int const POT = 7;
 
+void setup() {
+  Serial.begin(9600); //
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  for (int a = 8; a <= 12; ++a) {
-    digitalWrite(a, HIGH);
-    delay(250);
-    digitalWrite(a, LOW);
-    delay(250);
-  }
+  int potValue = analogRead(POT);
+  int Val = map(potValue, 0, 1023, 0, 255);
+  analogWrite(9, Val);
+  Serial.println(Val);
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
 // END FILE
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
-
