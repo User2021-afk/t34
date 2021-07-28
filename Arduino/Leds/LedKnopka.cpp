0b6ab8ef000985441c5e0e640d33f99545d9c8e5
David <davidmatry2007@gmail.com>
2021-07-28 14:21:44 +0300

    Added LedKnopka.cpp

---
+++ Arduino/Leds/LedKnopka.cpp
@@ -0,0 +1,13 @@
+const int PinKey = 2;      
+const int PinVT  = 12;
+bool      Action = 0;           
+
+void setup(){                   
+    pinMode(PinKey, INPUT);       
+    pinMode(PinVT, OUTPUT);  
+}
+
+void loop(){                     
+    Action = digitalRead(PinKey); 
+    digitalWrite(PinVT, Action); 
+}
